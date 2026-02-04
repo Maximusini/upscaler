33a40a551f3f7be96a3cdc4348361cb2295492b0
Maximusin <maxim.maxim.talankin@gmail.com>
2026-02-05 01:52:03 +0500

    refactor save_image function to include image quality parameters based on file extension

--- src/core/io_utils.py
+++ src/core/io_utils.py
@@ -8,7 +8,14 @@ def read_image(path):
     return img
 
 def save_image(path, img):
-    ext = os.path.splitext(path)[1]
-    success, buffer = cv2.imencode(ext, img)
+    ext = os.path.splitext(path)[1].lower()
+    
+    params = []
+    if ext in ['.jpg', '.jpeg']:
+        params = [cv2.IMWRITE_JPEG_QUALITY, 100]
+    elif ext == '.webp':
+        params = [cv2.IMWRITE_WEBP_QUALITY, 100]
+        
+    success, buffer = cv2.imencode(ext, img, params)
     if success:
         buffer.tofile(path)

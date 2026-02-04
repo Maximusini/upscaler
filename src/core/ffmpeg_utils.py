5e929e8280cd78ce83eb779b96287162877d8a2a
Maximusin <maxim.maxim.talankin@gmail.com>
2026-02-05 01:41:00 +0500

    refactor merge_audio function to improve error handling and logging during audio merging process

--- src/core/ffmpeg_utils.py
+++ src/core/ffmpeg_utils.py
@@ -1,12 +1,24 @@
 import subprocess
+import logging
 
 def merge_audio(video_no_audio:str, video_with_audio:str, output_path:str):
-    res = subprocess.run([
-        'ffmpeg', 
-        '-i', video_no_audio, 
-        '-i', video_with_audio, 
-        '-map', '0:v',
-        '-map', '1:a', 
-        '-c', 'copy', 
-        '-y', output_path
-    ], check=True)
+    try:
+        cmd = [
+            'ffmpeg', 
+            '-i', video_no_audio, 
+            '-i', video_with_audio, 
+            '-map', '0:v', 
+            '-map', '1:a', 
+            '-c', 'copy', 
+            '-y', output_path
+        ]
+        subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+        
+    except subprocess.CalledProcessError:
+        logging.warning('Audio stream not found or merge failed. Saving video without audio.')
+        try:
+            subprocess.run([
+                'ffmpeg', '-i', video_no_audio, '-c', 'copy', '-y', output_path
+            ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except Exception as e:
+            logging.error(f'Critical ffmpeg error: {e}')
